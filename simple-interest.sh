f9f3f835ec297cc8ca98f7136eeb88ab00806774
Nihat001 <57731973+Nihat001@users.noreply.github.com>
2024-04-19 16:54:17 +0200

    Create simple-interest.sh

---
+++ simple-interest.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Script to calculate simple interest
+# Usage: ./simple-interest.sh <principal> <rate> <time>
+
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <principal> <rate> <time>"
+    exit 1
+fi
+
+principal=$1
+rate=$2
+time=$3
+interest=$(echo "scale=2; $principal * $rate * $time / 100" | bc)
+echo "The simple interest is: $interest"
